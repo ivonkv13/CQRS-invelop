970174f6c6bd3250bc6cc6924be7a43362e76785
ivonkv13 <in.wk@protonmail.com>
2025-03-03 13:34:39 +0200

    Enable migrations on start up of the project and some refactoring

--- InvelopApp.Server/Program.cs
+++ InvelopApp.Server/Program.cs
@@ -4,6 +4,9 @@ using InvelopApp.Server.Infrastructure;
 using InvelopApp.Server.Middleware;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Validations;
+using Microsoft.OpenApi.Writers;
 using System.Reflection;
 
 namespace InvelopApp.Server
@@ -14,36 +17,15 @@ namespace InvelopApp.Server
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-            // Register MediatR
-            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-
-            builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-
-            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAll", policy =>
-                {
-                    policy.WithOrigins("https://127.0.0.1:51308", "https://localhost:51308")
-                          .AllowAnyMethod()
-                          .AllowAnyHeader();
-                });
-            });
-
+            builder.AddServices();
 
             var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                dbContext.Database.Migrate();
+            }
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
@@ -59,7 +41,10 @@ namespace InvelopApp.Server
 
             app.UseHttpsRedirection();
 
-            app.UseCors("AllowAll");
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseCors("AllowAll");
+            }
 
             app.UseAuthorization();
 
